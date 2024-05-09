bb4d9a8d4d5970b547d95a9df8929634653da84f
Shibly Meeran <shibme@shib.me>
2024-05-09 19:39:50 +0530

    adding prefix to wasm methods

--- app/wasm/main.go
+++ app/wasm/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	fmt.Println("Xipher Web Assembly!")
-	js.Global().Set("newSecretKey", newSecretKey())
-	js.Global().Set("pubKeyFromPrivKey", pubKeyFromPrivKey())
-	js.Global().Set("pubKeyFromPassword", pubKeyFromPassword())
-	js.Global().Set("encryptStr", encryptStr())
-	js.Global().Set("decryptStrWithSecretKey", decryptStrWithSecretKey())
-	js.Global().Set("decryptStrWithPassword", decryptStrWithPassword())
+	js.Global().Set("xipherNewSecretKey", newSecretKey())
+	js.Global().Set("xipherPubKeyFromPrivKey", pubKeyFromPrivKey())
+	js.Global().Set("xipherPubKeyFromPassword", pubKeyFromPassword())
+	js.Global().Set("xipherEncryptStr", encryptStr())
+	js.Global().Set("xipherDecryptStrWithSecretKey", decryptStrWithSecretKey())
+	js.Global().Set("xipherDecryptStrWithPassword", decryptStrWithPassword())
 	<-make(chan struct{})
 }
